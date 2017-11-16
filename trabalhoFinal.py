95e1b092809b4647e44ba360c5bb8fa953c49f83
Natália Nunes <30706662+natnvieira7@users.noreply.github.com>
2017-11-16 19:13:17 -0200

    Create trabalhoFinal.py

---
+++ trabalhoFinal.py
@@ -0,0 +1,15 @@
+#Codigo em teste
+
+arq = open("arquivo.dat", "rb")
+
+tamanho = arq.tel(arq.seek (0, seek_end))
+
+lista = None
+[None]*255
+
+for lista in arq:
+
+  caracter = arq.read(1)
+  pos = ord(caracter)
+  
+  lista(pos) = (count == count + 1)
