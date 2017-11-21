fd37f5425db5f1959807c7d8b5499e50d914d17f
Natália Nunes <30706662+natnvieira7@users.noreply.github.com>
2017-11-21 12:04:01 -0200

    Update trabalhoFinal.py

--- trabalhoFinal.py
+++ trabalhoFinal.py
@@ -2,26 +2,28 @@
 
 import glob
 
-glob.glob ('*.*')  #Lê todos os arquivos de uma pasta
-
-for arq in glob:    # Para cada arquivo no glob faça
-  
-  lista = None # cria uma lista vazia
-  [None]*255   # insere 255 espaços na lista
-
-  for x in arq: # para cada 
-    
-    print x
-    caracter = arq.read(1)
-    contador (caracter)
-  
-  for y in lista:
-    
-    print chr(y)
-    print y
-  
-def contador(char x):
-  
+def contador(caracter, lista):
+      
   pos = ord(caracter)
-  lista(pos) = lista(pos) + 1
+  lista[pos] = lista[pos] + 1
+
+glob = glob.glob("/home/gabriel/Imagens/*.*") #alterar o caminho da pasta
+
+for arq in glob:   
+  arqui = open(arq, "r")
+  print (arq)  
+  lista = [0] * 255 # cria uma lista vazia
+
+  x = arqui.read(1)
+  while len(x) != 0:
+
+    #print (x)
+    contador (x, lista)
+    x = arqui.read(1)
+
+  for y in lista:
+        
+    #print (chr(y)) #chr nao esta funcionando
+    print (y)
+  
   
