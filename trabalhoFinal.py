b8aab13af75f797914a3bad4ea25ef0fac48d42e
Natália Nunes <30706662+natnvieira7@users.noreply.github.com>
2017-11-22 19:47:58 -0200

    Update trabalhoFinal.py

--- trabalhoFinal.py
+++ trabalhoFinal.py
@@ -12,7 +12,7 @@ glob = glob.glob("/home/gabriel/Imagens/*.*") #alterar o caminho da pasta
 for arq in glob:   
   arqui = open(arq, "r")
   print (arq)  
-  lista = [0] * 255 # cria uma lista vazia
+  lista = [0] * 256 # cria uma lista vazia
 
   x = arqui.read(1)
   while len(x) != 0:
@@ -21,9 +21,15 @@ for arq in glob:
     contador (x, lista)
     x = arqui.read(1)
 
-  for y in lista:
+  for i in range (0, len(lista)):
+            
+            if i<32 or i>126:
+                  caracter = "<" + str(i) + ">"
+            else:
+                  caracter=chr(i)
+                  
+            print (caracter+ " " + str(lista[i]))
         
-    #print (chr(y)) #chr nao esta funcionando
-    print (y)
+  
   
   
